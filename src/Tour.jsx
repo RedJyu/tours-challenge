bc5e5f986b0974fd6b66dced13cf8dda0b45ee18
RedJyu <piotr.stezycki@protonmai.com>
2023-09-07 15:00:18 +0200

    readmore btn finished

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+
 function Tour({ id, image, info, name, price, removeTours }) {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className='single-tour'>
       <img src={image} alt={name} className='img' />
       <span className='tour-price'>${price}</span>
       <div className='tour-info'>
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : info.substring(0, 200)}
+          <button
+            type='button'
+            className='info-btn'
+            // remember too use function in on click!!!
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            read more
+          </button>
+        </p>
+
         <button
           type='button'
           className='btn btn-block delete-btn'
