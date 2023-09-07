5ee014c5cd4b6d985eebc8028564005d1f58d54e
RedJyu <piotr.stezycki@protonmai.com>
2023-09-07 13:41:55 +0200

    tours challenge

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Loading from './Loading';
+import { useEffect, useState } from 'react';
+const url = 'https://course-api.com/react-tours-project';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return 'test';
+};
+export default App;
